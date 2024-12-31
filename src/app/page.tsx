e2c1a1d179a4d1513b22bc55d7a81b2de327fd1e
Rajesh97266 <rajesh97266@gmail.com>
2024-12-31 16:08:53 +0530

    home image added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 
+import Image from "next/image";
+import homeImg from "public/home.jpg";
+
 
 
 
 export default function Home() {
   return (
-<div>
- 
-  Home Page
-</div>
+    <div>
+      HomePage
+      <div className="absolute -z-10 inset-0">
+        <Image src={homeImg} alt="home" fill style={{ objectFit: "cover" }} />
+      </div>
+    </div>
   );
 }
