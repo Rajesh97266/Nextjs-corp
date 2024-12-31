8b73901787757c75b27ca4a2e233a29eaf28bc6c
Rajesh97266 <rajesh97266@gmail.com>
2024-12-31 12:46:51 +0530

    pages linked

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link";
+
 
 
 export default function Home() {
   return (
 <div>
+  <div>
+    <Link href="/scale">Scale</Link>
+    <Link href="/reliability">Reliability</Link>
+    <Link href="/performance">Performance</Link>
+  </div>
   Home Page
 </div>
   );
